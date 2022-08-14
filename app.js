bbbf657a35e05b9e8d1a45303e749a45b0116e3d
Mikko Hyyryläinen <mikko.e.hyyrylainen@gmail.com>
2022-08-14 22:31:52 +0300

    change default index

--- app.js
+++ app.js
@@ -20,12 +20,12 @@ mongoose.connect(config.MONGODB_URI)
   })
 
 morgan.token('request-body', (req) => JSON.stringify(req.body))
-app.use(express.static('build'))
 
 app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :response-time ms :res[content-length] :request-body'))
 app.use(middleware.tokenExtractor)
+app.use(express.static('build'))
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
