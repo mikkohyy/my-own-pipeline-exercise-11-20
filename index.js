50d76c50bf6d07f7ccc67aeaec74a9c9801f8a3c
Mikko Hyyryläinen <mikko.e.hyyrylainen@gmail.com>
2022-08-16 20:50:03 +0300

    fix lint

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const app = require('./app')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
-// a new change
-
-const server = http.createServer(app);
+const server = http.createServer(app)
 
 server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
