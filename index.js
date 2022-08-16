9a7721679c3dc8e9c84ee67fe4046f2d2471f3ea
Mikko Hyyryläinen <mikko.e.hyyrylainen@gmail.com>
2022-08-16 20:33:50 +0300

    break lint

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const logger = require('./utils/logger')
 
 // a new change
 
-const server = http.createServer(app)
+const server = http.createServer(app);
 
 server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
